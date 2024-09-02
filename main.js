3ea889950fcae197cf99f19abe45896e0ae7ac9a
jose-lucas <joselucas.dev@gmail.com>
2024-09-02 18:51:20 -0300

    feat: add Main if you  won't to run One by one

---
+++ main.js
@@ -0,0 +1,17 @@
+const firstQuestion = require('./questions/firstQuestion');
+const secondQuestion = require("./questions/secondQuestion");
+const thirdQuestion = require("./questions/thirdQuestion");
+const fourthQuestion = require("./questions/fourthQuestion");
+const fifthQuestion = require("./questions/fifthQuestion");
+
+
+
+firstQuestion()
+
+secondQuestion(8)
+
+thirdQuestion(1270)
+
+fourthQuestion()
+
+fifthQuestion('approved')
